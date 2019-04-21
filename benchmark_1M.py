81e0d9cc5e8627a45ed2d203ff6781f38ed8cccb
frkhit <frkhit@gmail.com>
2019-04-21 18:21:33 +0800

    增加百万(6M)词向量性能对比脚本和结果

--- benchmark_1M.py
+++ benchmark_1M.py
@@ -4,7 +4,25 @@
 
 词向量:
 - 规模: 6115353 x 64D
-- 来源:
+- 来源: [自然语言处理中句子相似度计算的几种方法](https://cuiqingcai.com/6101.html)提供的[news_12g_baidubaike_20g_novel_90g_embedding_64.bin](https://pan.baidu.com/s/1TZ8GII0CEX32ydjsfMc0zw)
+
+使用(下面代码可能有错误, 请自行修改):
+
+```
+# 编辑Dockerfile
+echo 'FROM frkhit/benchmark-word2vec:latest
+COPY news_12g_baidubaike_20g_novel_90g_embedding_64.bin ./word_vec_1m.bin
+COPY benchmark_1M.py benchmark.py ./
+ENTRYPOINT ["python"]
+CMD ["-u", "benchmark_1M.py"]
+' > Dockerfile
+
+# docker build
+docker build -t benchmark:latest .
+
+# docker run
+docker logs -f $(docker run -d benchmark:latest)
+```
 
 测试结果:
 - faiss: load index, 31.92s; search 100 times by word, 209.59s; search 100 times by vec, 215.94s
@@ -13,8 +31,14 @@
 """
 from __future__ import absolute_import
 
+import pickle
+import time
+
 import gensim
+import numpy as np
+import os
 from pyxtools import global_init_logger
+from pyxtools.faiss_tools import faiss
 
 from benchmark import FaissBenchmark, GensimBenchmark
 
@@ -40,7 +64,6 @@ class GensimBenchmark1M(Mixin, GensimBenchmark):
     def __init__(self):
         super(GensimBenchmark1M, self).__init__()
         self.word_vec_model_file = "word_vec_1m.bin"
-        self.dimension = 64
 
 
 class FaissBenchmark1M(Mixin, FaissBenchmark):
@@ -51,7 +74,54 @@ class FaissBenchmark1M(Mixin, FaissBenchmark):
         self.word_vec_model_file = "word_vec_1m.bin"
         self.faiss_index_file = "./faiss_1m.index"
         self.faiss_index_detail_pkl = "./faiss_1m.pkl"
-        self.dimension = 64
+
+        # faiss setting
+        self._faiss_factory = "IMI2x10,Flat"
+        self.n_probe = 8096
+
+    def prepare(self):
+        """ 将Gensim 版本的模型转化为Faiss模型 """
+        super(FaissBenchmark, self).prepare()
+
+        # turn model from gensim to faiss index
+        if os.path.exists(self.faiss_index_file) and os.path.exists(self.faiss_index_detail_pkl):
+            return
+
+        # load model to dict
+        self.logger.info("loading model...")
+        time_start = time.time()
+        gensim_model = self.load_pre_trained_model()
+        model_size = len(gensim_model.vocab)
+        self.dimension = gensim_model.vector_size
+        feature = np.zeros(shape=(model_size, self.dimension), dtype=np.float32)
+        word_list = [word for word in gensim_model.vocab]
+        for i, word in enumerate(word_list):
+            feature[i] = gensim_model.get_vector(word)  # not normed
+        self.logger.info("success to load index! Cost {} seconds!".format(time.time() - time_start))
+
+        # train faiss index
+        normed_feature = feature / np.linalg.norm(feature, axis=1, keepdims=True)
+        faiss_index = faiss.index_factory(self.dimension, self._faiss_factory)
+        self.logger.info("training index...")
+        time_start = time.time()
+        faiss_index.train(normed_feature)  # nb * d
+        faiss_index.add(normed_feature)
+        self.logger.info("success to train index! Cost {} seconds!".format(time.time() - time_start))
+
+        # save in file
+        faiss.write_index(faiss_index, self.faiss_index_file)
+        with open(self.faiss_index_detail_pkl, "wb") as f:
+            pickle.dump((word_list, feature), f)
+
+    def search(self):
+        """ search similar words """
+        self._model.nprobe = self.n_probe
+        super(FaissBenchmark1M, self).search()
+
+    def vec_search(self):
+        """ 直接使用词向量搜索 """
+        self._model.nprobe = self.n_probe
+        super(FaissBenchmark1M, self).vec_search()
 
 
 if __name__ == '__main__':
